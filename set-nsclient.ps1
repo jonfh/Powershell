a233b13107c1c5517c6e38c6a8dbf0020b9810d5
jonfh <jon_hv@hotmail.com>
2016-05-29 22:48:54 +0200

    set-nsclient error handling

--- set-nsclient.ps1
+++ set-nsclient.ps1
@@ -36,37 +36,45 @@
                 Write-Verbose "connecting to $_.IP"
                 if ($namelog) 
                 {
-                    "connected to $_.IP" | Out-File $logfile -Append
+                    "connecting to $_.IP" | Out-File $logfile -Append
                 } 
                 try 
                 {
-                    $session = New-PSSession -ComputerName $_.IP -Credential $credentials
+                    $session = New-PSSession -ComputerName $_.IP -Credential $credentials -EA 'Stop'
                     Write-Verbose "connected to $_.IP"
+                    "connected to $_.IP" | Out-File $logfile -Append
                     $core_path = "C:\GitHub\check_cores.ps1"
                     $sql_path = "C:\GitHub\get-SqlServerKeys.ps1"
                     Write-Verbose "Copying files to $_.IP"
                     if ($namelog) {
-                        "Copying files to $_.IP" | Out-File $logfile -Append
+                        "Copying $core_path to $_.IP" | Out-File $logfile -Append
                     } 
-                    try 
-                    {
-                        Copy-Item -Destination 'C:\Program Files\NSClient++\scripts\' -path $core_path  -toSession $session
+                    #try 
+                    #{
+                        Write-Verbose "Copying $core_path to $_.IP"
+                        Copy-Item -Destination 'C:\Program Files\NSClient++\scripts\' -path $core_path -toSession $session -EA 'Stop'
                         Write-Verbose "$core_path copied to $_"
-                    } catch 
-                    {
-                        Write-Verbose "copying file $core_path to $_.IP failed"
-                        "ERROR: copying file $core_path to $_.IP failed" | Out-File $logfile -Append
+                        "$core_path copied to $_" | Out-File -Append
+                    #} catch 
+                    #{
+                     #   Write-Verbose "copying file $core_path to $_.IP failed"
+                      #  "ERROR: copying file $core_path to $_.IP failed" | Out-File $logfile -Append
+                    #}
+                    if ($namelog) {
+                        "Copying $sql_path to $_.IP" | Out-File $logfile -Append
                     }
-                    try 
-                    {
-                        copy-Item -Destination 'C:\Program Files\NSClient++\scripts\' -path $sql_path -toSession $session
+                    #try 
+                    #{
+                        Write-Verbose "Copying $sql_path to $_.IP"
+                        copy-Item -Destination 'C:\Program Files\NSClient++\scripts\' -path $sql_path -toSession $session -EA 'Stop'
                         Write-Verbose "$sql_path copied to $_"
-                    }catch 
-                    {
-                        Write-Verbose "copying file $sql_path to $_.IP failed"
-                        "ERROR: copying file $sql_path to $_.IP failed" | Out-File $logfile -Append
-                    }
-                    Write-Verbose "editing NSClient.ini on $_.IP"
+                        "$sql_path copied to $_" | Out-File -Append
+                    #}catch 
+                    #{
+                     #   Write-Verbose "copying file $sql_path to $_.IP failed"
+                      #  "ERROR: copying file $sql_path to $_.IP failed" | Out-File $logfile -Append
+                    #}
+                        Write-Verbose "editing NSClient.ini on $_.IP"
                       if ($namelog) 
                       {
                         "editing NSClient.ini on $_.IP" | Out-File $logfile -Append
@@ -94,9 +102,10 @@
 
                         # Write content of $NewContent varibale back to file
                         $NewContent | Out-File -FilePath 'c:\program files\nsclient++\nsclient.ini' -Encoding Default -Force
-                        }
+                        } -EA 'Stop'
                     } catch
-                    {
+                    { 
+                        Write-Verbose "ERROR: editing NSClient.ini on $_.IP failed"
                        "ERROR: editing NSClient.ini on $_.IP failed" | Out-File $logfile -Append
                     }
 
@@ -123,21 +132,26 @@
 
                             # Write content of $NewContent varibale back to file
                             $NewContent | Out-File -FilePath 'c:\program files\nsclient++\nsclient.ini' -Encoding Default -Force
-                        }
+                        } -EA 'Stop'
                     } catch
                     {
-                         "ERROR: editing NSClient.ini on $_ failed" | Out-File $logfile -Append
+                         Write-Verbose "ERROR: editing NSClient.ini on $_.IP failed"
+                       "ERROR: editing NSClient.ini on $_.IP failed" | Out-File $logfile -Append
                     }
                     try 
                     {
+                        "restarting nscp" |Out-File $logfile -append
+                        Write-Verbose "restarting nscp"
                         Invoke-Command -Session $session -ScriptBlock {restart-service nscp}
                     } catch 
                     {
                         "Error: restarting nscp on $_ failed" |Out-File $logfile -Append
+                        Write-Verbose " restarting nscp on $_ failed"
                     }
                 } catch 
                 {
-                    "Error: could not connect to server $_ " | Out-File $logfile -Append
+                    #"Error: could not connect to server $_ " | Out-File $logfile -Append
+                    Write-Verbose "ERROR: could not connect to server"
                 }
             }
         }
