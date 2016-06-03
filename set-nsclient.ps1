8990d5cbec445ef19fe2db5403a1a72371cefef1
jonfh <jon_hv@hotmail.com>
2016-06-03 23:34:23 +0200

    improved logging

--- set-nsclient.ps1
+++ set-nsclient.ps1
@@ -1,6 +1,5 @@
 ﻿function set-nsclient {
-
-[CmdletBinding(SupportsShouldProcess=$True,ConfirmImpact='Medium')]
+    [CmdletBinding(SupportsShouldProcess=$True,ConfirmImpact='Medium')]
     param(
         
 	    [string]$servers,
@@ -13,10 +12,15 @@
         {
             Write-Verbose "Creating log file"
             $i =0
+            $j =0
+            
             DO {
+                $errorLogFile="c:\github\Error-$j.log"
                 $logfile = "c:\github\servers-$i.log"
                 $i++
-             } while (Test-Path $logfile)
+                $j++
+             }
+             while (Test-Path $logfile)
              Write-Verbose "logging in $logfile"
          }
          else 
@@ -29,78 +33,142 @@
         
         $filepath = $servers
         $user = $username
+        $connectionFailures =0
         $credentials = Get-Credential($user)
         $servere = Import-Csv $filepath | ForEach-Object {
+        $ip = $_ -replace "@{IP=",""
+        $ip = $ip -replace "}",""
+        $connectionFailure = "FALSE"
+        
             if($PSCmdlet.ShouldProcess($_.IP)) 
             {
-                Write-Verbose "connecting to $_"
+                Write-Verbose "connecting to $ip"
                 if ($namelog) 
                 {
-                    "connecting to $_" | Out-File $logfile -Append
+                    "INFO: connecting to $ip" | Out-File $logfile -Append
                 } 
                 try 
                 {
                     $session = New-PSSession -ComputerName $_.IP -Credential $credentials -EA 'Stop'
-                } catch 
-                {
-                    "Error: could not connect to server $_ " | Out-File $logfile -Append
-                    Write-Verbose "ERROR: could not connect to server"
-                }
-                    Write-Verbose "connected to $_"
-                    "connected to $_.IP" | Out-File $logfile -Append
+         
+                    #if ($connectionFailure) {continue}
+                    Write-Verbose "connected to $ip"
+                    "INFO: connected to $ip" | Out-File $logfile -Append
                     $core_path = "C:\GitHub\check_cores.ps1"
                     $sql_path = "C:\GitHub\get-SqlServerKeys.ps1"
                     $serial_path="C:\GitHub\get-serialnumber.ps1"
-                    Write-Verbose "Copying files to $_"
+                    $os_path="C:\GitHub\get-OS.ps1"
+                    Write-Verbose "Copying files to $ip"
                     if ($namelog) {
-                        "Copying $core_path to $_" | Out-File $logfile -Append
+                        "INFO: Copying $core_path to $ip" | Out-File $logfile -Append
+                    }
+
+                    try 
+                    {
+                        Write-Verbose "INFO: Copying $os_path to $ip"
+                        Copy-Item -Destination 'C:\Program Files\NSClient++\scripts\' -path $os_path -toSession $session 
+                        Write-Verbose "INFO: $os_path copied to $ip"
+                        "INFO: $os_path copied to $ip" | Out-File $logfile -Append
+                    } catch 
+                    {
+                        Write-Verbose $date  "ERROR: copying file $core_path to $ip failed"
+                        "$date ERROR: copying file $core_path to $ip failed" | Out-File $errorLogFile -Append
+                        "$date ERROR: copying file $core_path to $ip failed" | Out-File $logfile -Append
                     }
                     
                     try 
                     {
-                        Write-Verbose "Copying $serial_path to $_"
+                        Write-Verbose "INFO: Copying $serial_path to $ip"
                         Copy-Item -Destination 'C:\Program Files\NSClient++\scripts\' -path $serial_path -toSession $session 
-                        Write-Verbose "$serial_pathh copied to $_"
-                        "$serial_path copied to $_" | Out-File $logfile -Append
+                        Write-Verbose "INFO: $serial_path copied to $ip"
+                        "INFO: $serial_path copied to $ip" | Out-File $logfile -Append
                     } catch 
                     {
-                        Write-Verbose "copying file $core_path to $_ failed"
-                        "ERROR: copying file $core_path to $_ failed" | Out-File $logfile -Append
+                        Write-Verbose $date  "ERROR: copying file $core_path to $ip failed"
+                        "$date ERROR: copying file $core_path to $ip failed" | Out-File $errorLogFile -Append
+                        "$date ERROR: copying file $core_path to $ip failed" | Out-File $logfile -Append
                     }
                      
                     try 
                     {
-                        Write-Verbose "Copying $core_path to $_"
+                        Write-Verbose "INFO: Copying $core_path to $ip"
                         Copy-Item -Destination 'C:\Program Files\NSClient++\scripts\' -path $core_path -toSession $session 
-                        Write-Verbose "$core_path copied to $_"
-                        "$core_path copied to $_" | Out-File $logfile -Append
+                        Write-Verbose "INFO: $core_path copied to $ip"
+                        "INFO: $core_path copied to $ip" | Out-File $logfile -Append
                     } catch 
                     {
-                        Write-Verbose "copying file $core_path to $_ failed"
-                        "ERROR: copying file $core_path to $_ failed" | Out-File $logfile -Append
+                        Write-Verbose $date  "ERROR: copying file $core_path to $ip failed"
+                        "$date ERROR: copying file $core_path to $ip failed" | Out-File $errorLogFile -Append
+                        "$date ERROR: copying file $core_path to $ip failed" | Out-File $logfile -Append
                     }
                     if ($namelog) {
-                        "Copying $sql_path to $_" | Out-File $logfile -Append
+                        "INFO: Copying $sql_path to $ip" | Out-File $logfile -Append
                     }
                     try 
                     {
-                        Write-Verbose "Copying $sql_path to $_.IP"
+                        Write-Verbose "INFO: Copying $sql_path to $ip"
                         copy-Item -Destination 'C:\Program Files\NSClient++\scripts\' -path $sql_path -toSession $session 
-                        Write-Verbose "$sql_path copied to $_"
-                        "$sql_path copied to $_" | Out-File $logfile -Append
+                        Write-Verbose "INFO: $sql_path copied to $ip"
+                        "INFO: $sql_path copied to $ip" | Out-File $logfile -Append
                     }catch 
                     {
-                       Write-Verbose "copying file $sql_path to $_ failed"
-                        "ERROR: copying file $sql_path to $_ failed" | Out-File $logfile -Append
+                        Write-Verbose $date  "ERROR: copying file $sql_path to $ip failed"
+                        "$date ERROR: copying file $sql_path to $ip failed" | Out-File $errorLogFile -Append
+                        "$date ERROR: copying file $sql_path to $ip failed" | Out-File $logfile -Append
                     }
-                        Write-Verbose "editing NSClient.ini on $_"
-                      if ($namelog) 
-                      {
-                        "editing NSClient.ini on $_" | Out-File $logfile -Append
-                      }
+                    Write-Verbose "INFO: editing NSClient.ini on $ip"
+                    if ($namelog) 
+                    {
+                        "INFO: editing NSClient.ini on $ip" | Out-File $logfile -Append
+                    }
+                        
+                    try 
+                    { 
+                        Invoke-Command -Session $session -ScriptBlock{Set-ExecutionPolicy remotesigned}
+                    }
+                    catch
+                    {
+                        
+                    }
+
+
                     try 
                     {
-                       Invoke-Command -Session $session -ScriptBlock {$File = 'c:\program files\nsclient++\nsclient.ini'
+                        Invoke-Command -Session $session -ScriptBlock {$File = 'c:\program files\nsclient++\nsclient.ini'
+                                    # Process lines of text from file and assign result to $NewContent variable
+                        $NewContent = Get-Content -Path $File |
+                        ForEach-Object {
+                            # If line matches regex
+                            if($_ -match ('^' + [regex]::Escape('[/settings/external scripts/scripts]')))
+                            {
+                                # Output this line to pipeline
+                                $_
+
+                                # And output additional line right after it
+                                'check_os=cmd /c echo scripts\get-os.ps1; exit($lastexitcode) | powershell.exe  -command -'
+                            }elseif ($_ -notmatch ('^' + [regex]::Escape('check_os=cmd /c echo scripts\get-os.ps1; exit($lastexitcode) | powershell.exe  -command -'))) # If line doesn't matches regex
+                            {
+                                # Output this line to pipeline
+                                $_
+                            }
+                        }
+
+                        # Write content of $NewContent varibale back to file
+                        $NewContent | Out-File -FilePath 'c:\program files\nsclient++\nsclient.ini' -Encoding Default -Force
+                        } -EA 'Stop'
+                    } catch
+                    { 
+                        
+                        Write-Verbose $date  "ERROR: editing NSClient.ini on $ip failed"
+                        "$date ERROR: editing NSClient.ini on $ip failed" | Out-File $errorLogFile -Append
+                        "$date ERROR: editing NSClient.ini on $ip failed" | Out-File $logfile -Append
+                    }
+
+
+             
+                    try 
+                    {
+                        Invoke-Command -Session $session -ScriptBlock {$File = 'c:\program files\nsclient++\nsclient.ini'
                                     # Process lines of text from file and assign result to $NewContent variable
                         $NewContent = Get-Content -Path $File |
                         ForEach-Object {
@@ -124,8 +192,9 @@
                         } -EA 'Stop'
                     } catch
                     { 
-                        Write-Verbose "ERROR: editing NSClient.ini on $_ failed"
-                       "ERROR: editing NSClient.ini on $_ failed" | Out-File $logfile -Append
+                        
+                        Write-Verbose $date  "ERROR: editing NSClient.ini on $ip failed"
+                        "$date ERROR: editing NSClient.ini on $ip failed" | Out-File $errorLogFile -Append
                     }
 
                     try {
@@ -154,13 +223,14 @@
                         } -EA 'Stop'
                     } catch
                     {
-                         Write-Verbose "ERROR: editing NSClient.ini on $_ failed"
-                       "ERROR: editing NSClient.ini on $_ failed" | Out-File $logfile -Append
+                            Write-Verbose $date  "ERROR: editing NSClient.ini on $ip failed"
+                        "$date ERROR: editing NSClient.ini on $ip failed" | Out-File $errorLogFile -Append
+                        "$date ERROR: editing NSClient.ini on $ip failed" | Out-File $logfile -Append
                     }
 
-                     try 
+                    try 
                     {
-                       Invoke-Command -Session $session -ScriptBlock {$File = 'c:\program files\nsclient++\nsclient.ini'
+                        Invoke-Command -Session $session -ScriptBlock {$File = 'c:\program files\nsclient++\nsclient.ini'
                                     # Process lines of text from file and assign result to $NewContent variable
                         $NewContent = Get-Content -Path $File |
                         ForEach-Object {
@@ -172,7 +242,7 @@
 
                                 # And output additional line right after it
                                 'check_serial=cmd /c echo scripts\get-serialnumber.ps1; exit($lastexitcode) | powershell.exe  -command -'
-                            }elseif ($_ -notmatch ('^' + [regex]::Escape('check_serial=cmd /c echo scripts\get-serialnumber.ps1; exit($lastexitcode) | powershell.exe  -command -'))) # If line doesn't matches regex
+                            }elseif ($_ -notmatch ('^' + [regex]::Escape('check_serial=cmd /c echo scripts\get-serialnumber.ps1; exit($lastexitcode) | powershell.exe  -command -')) -and $_ -notmatch ('^' + [regex]::Escape('check_serial=cmd /c echo scripts\check_serialnumber.ps1; exit($lastexitcode) | powershell.exe  -command -')) -and $_ -notmatch ('^' + [regex]::Escape('check_cores=cmd /c echo scripts\check_serialnumber.ps1; exit($lastexitcode) | powershell.exe  -command -'))) # If line doesn't matches regex
                             {
                                 # Output this line to pipeline
                                 $_
@@ -184,8 +254,9 @@
                         } -EA 'Stop'
                     } catch
                     { 
-                        Write-Verbose "ERROR: editing NSClient.ini on $_ failed"
-                       "ERROR: editing NSClient.ini on $_ failed" | Out-File $logfile -Append
+                        Write-Verbose $date  "ERROR: editing NSClient.ini on $ip failed"
+                        "$date ERROR: editing NSClient.ini on $ip failed" | Out-File $errorLogFile -Append
+                        "$date ERROR: editing NSClient.ini on $ip failed" | Out-File $logfile -Append
                     }
 
 
@@ -194,18 +265,29 @@
 
                     try 
                     {
-                        "restarting nscp" |Out-File $logfile -append
-                        Write-Verbose "restarting nscp"
+                        "INFO: restarting nscp" |Out-File $logfile -append
+                        Write-Verbose "INFO: restarting nscp"
                         Invoke-Command -Session $session -ScriptBlock {restart-service nscp}
                     } catch 
                     {
-                        "Error: restarting nscp on $_ failed" |Out-File $logfile -Append
-                        Write-Verbose " restarting nscp on $_ failed"
+                        "Error: restarting nscp on $ip failed" |Out-File $errorLogFile -Append
+                        "Error: restarting nscp on $ip failed" |Out-File $logfile -Append
+                        Write-Verbose "ERROR restarting nscp on $ip failed"
                     }
+                } catch 
+                {
+                    $connectionFailure = "TRUE"
+                    $connectionFailures++                 
+                    $date = (get-date -f dd-MM-HH:mm:ss) 
+                    Write-Verbose "$date ERROR: could not connect to server $ip"
+                    "$date Error: could not connect to server $ip count $connectionFailures" | Out-File $errorLogFile -Append
+                    "$date Error: could not connect to server $ip count $connectionFailures" | Out-File $logfile -Append
+                    # continue
+                   
+                }
           
             }
         }
     }
     END {}
 }
-#set-nsclient -servers C:\GitHub\server.csv -username Administrator -namelog -Verbose
